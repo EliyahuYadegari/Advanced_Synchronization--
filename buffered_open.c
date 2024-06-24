7d9ab5f720d6f1372e0a8285f92d1b90f36f5729
Your Name <you@example.com>
2024-06-24 22:33:55 +0300

    little fix part3 + example main

--- buffered_open.c
+++ buffered_open.c
@@ -76,43 +76,18 @@ ssize_t buffered_write(buffered_file_t *bf, const void *buf, size_t count) {
     return bytes_written;
 }
 
-// ssize_t buffered_read(buffered_file_t *bf, void *buf, size_t count) {
-//     if (!bf || !buf) {
-//         errno = EINVAL;
-//         return -1;
-//     }
-
-//     size_t bytes_written = 0;   // how many bytes where written
-//     const char *buffer = (const char *)buf;
-
-//     size_t space_left = bf->write_buffer_size - bf->write_buffer_pos;   // how much space left in the write buffer
-//     if(count > space_left){
-//         // flash the data frome the buffer to the file and then write to the empty buffer the new data
-//         if (buffered_flush(bf) == -1) {
-//             return -1;
-//         }
-//     }
-//     size_t to_write = count;
-//     // copy to_write from the input buffer to the write_buffer
-//     memcpy(bf->write_buffer + bf->write_buffer_pos, buffer + bytes_written, to_write);
-//     bf->write_buffer_pos += to_write;   // update the write_buffer_pos
-//     bytes_written += to_write;
-
-//     return bytes_written;
-// }
-
 ssize_t buffered_read(buffered_file_t *bf, void *buf, size_t count) {
     if (!bf || !buf) {
         errno = EINVAL;
         return -1;
     }
-
-    if (bf->write_buffer_pos > 0) {
-        if (buffered_flush(bf) == -1) {
-            return -1;
-        }
-    }
-
+    
+    // if (bf->write_buffer_pos > 0) {
+    //     if (buffered_flush(bf) == -1) {
+    //         return -1;
+    //     }
+    // }
+    
     size_t bytes_read = 0;
     char *buffer = (char *)buf;
 
@@ -126,14 +101,12 @@ ssize_t buffered_read(buffered_file_t *bf, void *buf, size_t count) {
             }
             bf->read_buffer_pos = n;
         }
-
         size_t to_read = (count < bf->read_buffer_pos) ? count : bf->read_buffer_pos;
         memcpy(buffer + bytes_read, bf->read_buffer + (bf->read_buffer_size - bf->read_buffer_pos), to_read);
         bf->read_buffer_pos -= to_read;
         bytes_read += to_read;
         count -= to_read;
     }
-
     return bytes_read;
 }
 
